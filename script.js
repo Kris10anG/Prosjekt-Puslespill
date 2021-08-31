45c86dd8691718820cf8cbfd0e968e2016a8fdc5
Kris10anG <kristiang@getacademy.no>
2021-08-31 13:26:17 +0200

    done

---
+++ script.js
@@ -0,0 +1,46 @@
+var storeClass;
+var selected = false;
+var storedPieces = [];
+
+// Right side div click
+function pick(element) {
+    let pieceClass = element.className;
+    if (!selected && storedPieces.indexOf(pieceClass) === -1) {
+        storeClass = element.className;
+        element.style.opacity = 0.3;
+        selected = true;
+    }
+}
+
+// Left side div click
+function place(element) {
+    if (element.classList.length) {
+        let removeClass = element.className;
+        element.classList.remove(removeClass);
+        storedPieces.indexOf(removeClass) !== -1 && storedPieces.splice(storedPieces.indexOf(removeClass), 1);
+        document.querySelector("." + removeClass).style.opacity = 1;
+    }
+    if (storeClass) {
+        element.classList.add(storeClass);
+        selected = false;
+        storedPieces.push(storeClass);
+        storeClass = '';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//DU SKAL SITTE HER, juden schwein 
